77fadfae7eecd3d2ec4dccf462f4eb6726484d66
merinjoseph0906 <98261699+merinjoseph0906@users.noreply.github.com>
2022-03-22 13:03:59 +0530

    Add files via upload

---
+++ Append.java
@@ -0,0 +1,8 @@
+public class Append{
+ public static void main(String args[]){
+ String firstName="merin";
+ String lastName="joseph";
+ String fullName=firstName + lastName;
+ System.out.println(fullName);
+   }
+ }
---
+++ Result.java
@@ -0,0 +1,8 @@
+public class Result{
+	public static void main(String args[]){
+		int x=100;
+		int y=300;
+		int z=x + y;
+	    System.out.println(z);
+    }
+}
